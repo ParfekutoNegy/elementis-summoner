001a3459603f5acaaf74e41ebbc2841d2e853284
ParfekutoNegy <jitekomojiketa@hotmail.com>
2026-09-01 21:58:50 +0900

    20260901AM

--- sw.js
+++ sw.js
@@ -1,25 +1,113 @@
-const CACHE_NAME = "deck-app-v1";
+const CACHE_NAME = "elementis-summoner-v1";
+
 const FILES_TO_CACHE = [
+
     "./",
     "./index.html",
-    "./style.css",
-    "./script.js",
-    "./images/card-back.jpg",
-    "./images/grimoire.png"
+    "./top.css",
+    "./cards.js",
+    "./manifest.json"
+
 ];
+
+
+//======================================
 // インストール
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-    );
-});
-// フェッチ
-self.addEventListener("fetch", event => {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request);
-        })
-    );
-});
+//======================================
+
+self.addEventListener(
+    "install",
+    event => {
+
+        console.log(
+            "★ Service Worker インストール"
+        );
+
+        event.waitUntil(
+
+            caches.open(
+                CACHE_NAME
+            ).then(
+                cache => {
+
+                    return cache.addAll(
+                        FILES_TO_CACHE
+                    );
+
+                }
+            )
+
+        );
+
+    }
+);
+
+
+//======================================
+// 古いキャッシュ削除
+//======================================
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        console.log(
+            "★ Service Worker アクティベート"
+        );
+
+        event.waitUntil(
+
+            caches.keys().then(
+                cacheNames => {
+
+                    return Promise.all(
+
+                        cacheNames
+                            .filter(
+                                cacheName =>
+                                    cacheName !== CACHE_NAME
+                            )
+                            .map(
+                                cacheName =>
+                                    caches.delete(
+                                        cacheName
+                                    )
+                            )
+
+                    );
+
+                }
+            )
+
+        );
+
+    }
+);
+
+
+//======================================
+// ファイル取得
+//======================================
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            fetch(
+                event.request
+            ).catch(
+                () => {
+
+                    return caches.match(
+                        event.request
+                    );
+
+                }
+            )
+
+        );
+
+    }
+);
